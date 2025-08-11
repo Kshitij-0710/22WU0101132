f3a43bcf5f1ba0ca9795f28aeba5d3c177fe224b
Kshitij Moghe <kshitijmoghe10@gmail.com>
2025-08-11 11:31:17 +0530

    added api urls

--- Backend Test Submission/backend/backend/urls.py
+++ Backend Test Submission/backend/backend/urls.py
@@ -15,8 +15,13 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from .routing import router
+from shorturls.views import RedirectView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    *router.urls,
+    path('<str:shortcode>/', RedirectView.as_view(), name='redirect'),
 ]
+
