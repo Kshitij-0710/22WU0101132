b4284a5f9765fe92b565a8d60142bdf404ee4b84
Kshitij Moghe <kshitijmoghe10@gmail.com>
2025-08-11 12:19:27 +0530

    added stats page

--- Frontend Test Submission/app/src/App.jsx
+++ Frontend Test Submission/app/src/App.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import { Container, Typography } from '@mui/material';
 import ShortenerPage from './views/shortnerpage';
+import StatsPage from './views/statspage.jsx';
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
         </Typography>
         <Routes>
           <Route path="/" element={<ShortenerPage />} />
+          <Route path="/stats" element={<StatsPage />} />
         </Routes>
       </Container>
     </Router>
