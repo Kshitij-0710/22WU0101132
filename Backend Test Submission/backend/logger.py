7397e617765ec22f112dae54561e87d2984e566e
Kshitij Moghe <kshitijmoghe10@gmail.com>
2025-08-11 12:19:18 +0530

    auth token updated again

--- Backend Test Submission/backend/logger.py
+++ Backend Test Submission/backend/logger.py
@@ -4,7 +4,7 @@ import json
 
 LOG_API_URL = "http://20.244.56.144/evaluation-service/logs"
 
-AUTH_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiJrc2hpdGlqLm1vZ2hlXzIwMjZAd294c2VuLmVkdS5pbiIsImV4cCI6MTc1NDg5MzQ2MCwiaWF0IjoxNzU0ODkyNTYwLCJpc3MiOiJBZmZvcmQgTWVkaWNhbCBUZWNobm9sb2dpZXMgUHJpdmF0ZSBMaW1pdGVkIiwianRpIjoiZGRiNWQ1ZjktODI5Yi00YjYxLWFjNmMtODNjOWNhMzUwYmYwIiwibG9jYWxlIjoiZW4tSU4iLCJuYW1lIjoia3NoaXRpaiBtb2doZSIsInN1YiI6IjNiYzA2ZmQ0LTc3ZGQtNDM4Ni05MThmLThjNjI5MjVlOThjZiJ9LCJlbWFpbCI6ImtzaGl0aWoubW9naGVfMjAyNkB3b3hzZW4uZWR1LmluIiwibmFtZSI6ImtzaGl0aWogbW9naGUiLCJyb2xsTm8iOiIyMnd1MDEwMTEzMiIsImFjY2Vzc0NvZGUiOiJVTVhWUVQiLCJjbGllbnRJRCI6IjNiYzA2ZmQ0LTc3ZGQtNDM4Ni05MThmLThjNjI5MjVlOThjZiIsImNsaWVudFNlY3JldCI6ImpuQll5c1RZd05xY1ZFeFcifQ.FlyXYbIhitsV3CPRwcipHIovtJ71yaSZhGf8TFzNZ4Y"
+AUTH_TOKEN = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJNYXBDbGFpbXMiOnsiYXVkIjoiaHR0cDovLzIwLjI0NC41Ni4xNDQvZXZhbHVhdGlvbi1zZXJ2aWNlIiwiZW1haWwiOiJrc2hpdGlqLm1vZ2hlXzIwMjZAd294c2VuLmVkdS5pbiIsImV4cCI6MTc1NDg5NTYxNCwiaWF0IjoxNzU0ODk0NzE0LCJpc3MiOiJBZmZvcmQgTWVkaWNhbCBUZWNobm9sb2dpZXMgUHJpdmF0ZSBMaW1pdGVkIiwianRpIjoiZWYyOTg4NWMtNjQ4MC00ODM2LWExNzktMTIyOGRhODEyYjFjIiwibG9jYWxlIjoiZW4tSU4iLCJuYW1lIjoia3NoaXRpaiBtb2doZSIsInN1YiI6IjNiYzA2ZmQ0LTc3ZGQtNDM4Ni05MThmLThjNjI5MjVlOThjZiJ9LCJlbWFpbCI6ImtzaGl0aWoubW9naGVfMjAyNkB3b3hzZW4uZWR1LmluIiwibmFtZSI6ImtzaGl0aWogbW9naGUiLCJyb2xsTm8iOiIyMnd1MDEwMTEzMiIsImFjY2Vzc0NvZGUiOiJVTVhWUVQiLCJjbGllbnRJRCI6IjNiYzA2ZmQ0LTc3ZGQtNDM4Ni05MThmLThjNjI5MjVlOThjZiIsImNsaWVudFNlY3JldCI6ImpuQll5c1RZd05xY1ZFeFcifQ.d7zqkpSN3e8HReFtUu8TNfvvRe7ip2j1DI3N1edIgYs"
 
 def Log(level: str, package: str, message: str):
     """
@@ -32,6 +32,5 @@ def Log(level: str, package: str, message: str):
             print(f"sending backend logs: [{level.upper()}] {message}")
         else:
             print(f"error sending the logs status: {response.status_code}, Response: {response.text}")
-            
     except requests.exceptions.RequestException as e:
         print(f"Network error: {e}")
